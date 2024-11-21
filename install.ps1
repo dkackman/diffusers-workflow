c2d60451f1f6dfc5958d2b5b65dcc91ef607859a
Don Kackman <dkackman@gmail.com>
2024-11-21 01:19:42 +0000

    torachao quantization

--- install.ps1
+++ install.ps1
@@ -70,10 +70,10 @@ python -m venv venv
 python.exe -m pip install --upgrade pip
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-pip3 install torch==2.4.1 diffusers[torch] peft transformers accelerate safetensors controlnet_aux sentencepiece mediapipe torchsde bitsandbytes --index-url https://download.pytorch.org/whl/cu124
+pip3 install torch==2.4.1 diffusers[torch] peft transformers accelerate safetensors controlnet_aux sentencepiece mediapipe torchsde bitsandbytes torchao --index-url https://download.pytorch.org/whl/cu124
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-pip3 install aiohttp matplotlib moviepy opencv-python concurrent-log-handler qrcode protobuf imageio imageio-ffmpeg beautifulsoup4 soundfile jsonschema pyyaml
+pip3 install aiohttp matplotlib moviepy opencv-python concurrent-log-handler qrcode protobuf imageio imageio-ffmpeg beautifulsoup4 soundfile jsonschema
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 Write-Output "done"
