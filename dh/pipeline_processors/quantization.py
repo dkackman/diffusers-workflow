3f491b065fdda1c2763d18ce29ff9d408a17b72c
Don Kackman <dkackman@gmail.com>
2024-11-22 21:20:09 +0000

    small refactor

--- dh/pipeline_processors/quantization.py
+++ dh/pipeline_processors/quantization.py
@@ -1,9 +1,11 @@
 from torchao.quantization import quantize_ as torachao_quantize
 from optimum.quanto import freeze, quantize as quanto_quantize
 
+
 def quantize(component, quantization_definition):
     if quantization_definition is not None:
         quantization_library = quantization_definition["quantization_library"]
+        print(f"Quantizing {type(component).__name__} using {quantization_library}...")
         weights_type = quantization_definition["weights_type"]
 
         if quantization_library == "torchao":
