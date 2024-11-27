1673759c55974af7f1a2a3333c8656b70659d91d
Don Kackman <dkackman@gmail.com>
2024-11-27 03:21:20 +0000

    don't save outpuse without content type

--- dh/pipeline_processors/result.py
+++ dh/pipeline_processors/result.py
@@ -7,7 +7,7 @@ class Result:
     def __init__(self, result, iteration):
         self.result = result
         self.iteration = iteration
-        self.properties = iteration.get("result_properties", {"content_type": "image/png"})
+        self.properties = iteration.get("result_properties", {})
 
     def __repr__(self):
         return f"Result({self.result})"
@@ -35,8 +35,7 @@ class Result:
     
     # This method is used to guess the extension of the output file based on the content type.
     # It includes the leading "." in the extension.
-    def guess_extension(self):
-        content_type = self.properties["content_type"]
+    def guess_extension(self, content_type):
         ext = mimetypes.guess_extension(content_type)
         if ext is not None:
             return ext
@@ -47,18 +46,22 @@ class Result:
         return ""
 
     def save(self, output_dir, default_base_name):
-        file_base_name = self.properties.get("file_base_name", default_base_name)  
-        output_path = os.path.join(output_dir, f"{file_base_name}{self.guess_extension()}")
-                    
-        print(f"Saving result to {output_path}")
+        content_type = self.properties.get("content_type", None)
 
-        output = self.get_output()
-        content_type = self.properties.get("content_type", "")
-        if content_type.startswith("video"):
-            export_to_video(output, output_path, fps=self.properties.get("fps", 8))
+        # do not save if no result content_type is specified
+        if content_type is not None:
+            file_base_name = self.properties.get("file_base_name", default_base_name)  
+            output_path = os.path.join(output_dir, f"{file_base_name}{self.guess_extension(content_type)}")
+                        
+            print(f"Saving result to {output_path}")
 
-        elif content_type.startswith("audio"):
-            soundfile.write(output_path, output, self.properties.get("sample_rate", 44100))
+            output = self.get_output()
+            content_type = self.properties.get("content_type", "")
+            if content_type.startswith("video"):
+                export_to_video(output, output_path, fps=self.properties.get("fps", 8))
 
-        elif hasattr(output, 'save'):
-            output.save(output_path)        
+            elif content_type.startswith("audio"):
+                soundfile.write(output_path, output, self.properties.get("sample_rate", 44100))
+
+            elif hasattr(output, 'save'):
+                output.save(output_path)        
