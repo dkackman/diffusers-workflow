aa51d88baf2389fcd4fcfe87da074ca25cc3ea28
Don Kackman <dkackman@gmail.com>
2024-12-01 03:32:13 +0000

    collapse dispatch

--- dh/tasks/task.py
+++ dh/tasks/task.py
@@ -1,12 +1,22 @@
-from .task_dispatch import dispatch_task
+from .qr_code import get_qrcode_image
+from .image_processor_dispatch import process_image
 
 class Task:
     def __init__(self, task_definition):
         self.task_definition = task_definition
 
     def run(self, device_identifier):
-        task_output = dispatch_task(self.task_definition["command"], self.task_definition.get("arguments", {}))
-        return task_output
+        command = self.task_definition["command"]
+        kwargs = self.task_definition.get("arguments", {})
+                                             
+        if command == "qr_code":
+            return get_qrcode_image(**kwargs)
+
+        if "image" in kwargs:
+            return process_image(kwargs.pop("image"), command, device_identifier, kwargs)
+                    
+        raise ValueError(f"Unknown task {command}")
+
     
     @property
     def command(self):
