09a0118a01f147baa75cd0c3a429331aed5e68b4
Don Kackman <dkackman@gmail.com>
2024-12-29 03:55:38 +0000

    unit tests

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dh",
+    version="0.37.0",  # Matches version in dh/__init__.py
+    packages=find_packages(),
+    install_requires=[
+        "torch>=2.0.0",
+        "diffusers",
+        "transformers",
+        "qrcode",
+        "Pillow",
+        "jsonschema",
+        "concurrent-log-handler",
+        "controlnet-aux",
+        "soundfile",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "black",
+            "flake8",
+        ],
+    },
+    python_requires=">=3.8",
+    author="dkackman",
+    description="A helper library for diffusers workflows",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dkackman/diffusers-helper",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
