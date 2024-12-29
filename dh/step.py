0261cfeedeaeaa5f19f0b3f4c7af1559e1bdb130
Don Kackman <dkackman@gmail.com>
2024-12-29 19:58:03 +0000

    logging pattern

--- dh/step.py
+++ dh/step.py
@@ -1,11 +1,15 @@
 from .result import Result
 from .previous_results import get_iterations
+import logging
+
+logger = logging.getLogger("dh")
 
 
 class Step:
     def __init__(self, step_definition, default_seed):
         self.step_definition = step_definition
         self.default_seed = self.step_definition.get("seed", default_seed)
+        logger.debug(f"Initialized step: {self.name} with seed: {self.default_seed}")
 
     @property
     def name(self):
@@ -17,16 +21,15 @@ class Step:
 
             result = Result(self.step_definition.get("result", {}))
 
-            print(f"Running task {step_name}:{step_action.name}...")
+            logger.info(f"Running step {step_name}:{step_action.name}...")
             for arguments in get_iterations(
                 step_action.argument_template, previous_results
             ):
-                r = step_action.run(arguments, previous_pipelines)
-                result.add_result(r)
+                logger.debug(f"Running iteration with arguments: {arguments}")
+                result.add_result(step_action.run(arguments, previous_pipelines))
 
             return result
 
         except Exception as e:
-            print(f"Error running step {self.step_definition.get('name', 'unknown')}")
-            print(e)
+            logger.error(f"Error running step {self.name}: {str(e)}", exc_info=True)
             raise e
--- dh/validate.py
+++ dh/validate.py
@@ -1,17 +1,27 @@
 import argparse
 import os
 from .workflow import workflow_from_file
-
+from . import startup
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Validate a workflow from a file.")
     parser.add_argument(
         "file_name", type=str, help="The filespec of the workflow to validate"
     )
+
+    parser.add_argument(
+        "-l",
+        "--log_level",
+        type=str,
+        default="INFO",
+        help="Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)",
+    )
     args = parser.parse_args()
 
     if not os.path.exists(args.file_name):
         raise FileNotFoundError(f"File {args.file_name} does not exist")
+
+    startup(args.log_level)
 
     try:
         workflow = workflow_from_file(args.file_name, ".")
@@ -20,4 +30,3 @@ if __name__ == "__main__":
         print(f"Error validating workflow: {args.file_name}")
         exit(1)
 
-    print(f"Workflow {args.file_name} validated successfully")
